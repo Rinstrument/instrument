c66a852e078260093fea6c26a27f6f8c07c82b8b
Michael Kleinsasser <mkleinsa@umich.edu>
2023-03-10 09:24:06 -0500

    m

--- tests/simulation/evaluate_model.R
+++ tests/simulation/evaluate_model.R
@@ -19,13 +19,32 @@ retrieve_by_name = function(res, name) {
 # Evaluate simulation output using the standard metrics like mse, bias, etc...
 evaluate_model = function(res) {
 
+	n_sim = length(res)
+
 	# true values are equal across the res[[i]]'s since they were sampled once
 	truth = res[[1]][, 'true']
 
 	# posterior estimate table for all parameters
 	estimates = retrieve_by_name(res, 'mean')
 
-	
+	data.table::setnames(estimates, paste0('mean', 1:n_sim))
+
+	estimates = cbind(estimates, truth)
+
+	estimates[, .SD - true, .SDcols = grep('mean', colnames(estimates))]
+
+	estimates[, `:=`(avg_sim = rowMeans(.SD)), .SDcols = grep('mean', colnames(estimates))]
+
+	estimates[, lapply(.SD, \(x, y) {x - y}, y = avg_sim), .SDcols = grep('mean', colnames(estimates))]
+
+	estimates[, `:=`(new = .SD - 2), .SDcols = grep('mean', colnames(estimates))]
+
+	estimates[ , `:=`(avg_sim = rowMeans(.SD)), .SDcols = grep('mean', colnames(estimates))][
+						 , `:=`(bias = avg_sim - true)][
+						 , `:=`(sq_dev_mean = rowSums((.SD))), .SDcols = grep('mean', colnames(estimates))
+						 ][
+						 , `:=`(sq_dev_mean = rowSums((.SD - avg_sim)^2)), .SDcols = grep('mean', colnames(estimates))][
+						 , `:=`(bias_mcse = sqrt((1 / (n_sim*(n_sim-1)))*rowSums(.SD))), .SDcols = grep('mean', colnames(estimates))]
 
 	# mean of model estimates
 	est_mean_nres = est_sum_nres / n_res
